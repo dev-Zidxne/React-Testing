25172d85d35a6677c2ff251136385baf712ea065
Zidane Innis <zidaneinnis@gmail.com>
2023-02-19 16:04:17 -0500

    Testing User Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 describe("Greeting component", () => {
   test("renders hello World as a text", () => {
@@ -12,5 +13,38 @@ describe("Greeting component", () => {
     //Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders its good to see you when BEFORE button is pressed", () => {
+    render(<Greeting />);
+
+    const greeting = screen.getByText("It's good to see you");
+    expect(greeting).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if button is clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("Greeting Paragraph is gone when button is clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
